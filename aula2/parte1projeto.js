60986d34afa482ee9e4844e3bb941ae284087f77
GabrielMM182 <gmm_morais@hotmail.com>
2024-09-18 07:56:42 -0300

    add: git ignore e resolucao da tarefa

---
+++ aula2/parte1projeto.js
@@ -0,0 +1,19 @@
+const escolhaFeita = 0
+
+if (escolhaFeita === 0) {
+    console.log("saiu da aplicação");
+} else if (escolhaFeita === 1) {
+    console.log("cadastrar filme");
+} else if (escolhaFeita === 2) {
+    console.log("Listar filmes");
+} else if (escolhaFeita === 3) {
+    console.log("Calcular média");
+} else if (escolhaFeita === 4) {
+    console.log("Filtrar por filmes não vistos");
+} else if (escolhaFeita === 5) {
+    console.log("Excluir último filme cadastrado");
+} else if (escolhaFeita === 6) {
+    console.log("Excluir filme específico");
+} else {
+    console.log("opção invalida");
+}
